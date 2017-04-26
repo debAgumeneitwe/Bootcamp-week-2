24f4da591252ab695af6556755ba061351217a6d
Deborah Agumeneitwe <dagumeneitwe@gmail.com>
2017-04-26 11:20:16 +0300

    functionality of create toom successfully test

--- my_project_files/Dojo_room_allocation.py
+++ my_project_files/Dojo_room_allocation.py
@@ -1,66 +1,32 @@
-Class Dojo(object):
+class Dojo(object):
     def __init__(self,room_name,room_type):
         self.room_name = room_name
         self.room_type = room_type
-        all_rooms = {}
-        People = {}
-        initial_room_count = len(dojo.all_rooms)
+        self.all_rooms = []
+        self.People = []
+        self.initial_room_count = 0
+        self.office_rooms = []
+        self.living_rooms = []
         
         
-    def create_room_successfully(self):
-        self.room = Dojo()
         
-        while room_type == "office" or room_type == "living":
-            Raise ("Please input room name of your choice")
+    def create_room(self,room_name,room_type):
+        new_room = Dojo(room_name,room_type)
+        
+        if room_type == "office" or room_type == "living": 
             
-            if input = all_rooms.key:
-                print ("An office called {} has been successfully created!").format(room_name)
-            else:
-                Print ("Please input room name from the options")
+            if room_type == "office":
+                self.office_rooms.append(new_room)
+                    
+            elif room_type == "living":
+                self.living_rooms.append(new_room)
+               
+        else:
+            return ("Room type has to be either living or office")
         
-        room_count = initial_room_count + 1
+        self.all_rooms = self.office_rooms + self.living_rooms
+           
+    
         
        
-        
-    def add_person(self):
-        accomodation option = N
-        People.append(person)
-        Raise ("Neil has been successfully added")
-        
-        if position == fellow and accomodation_option == N:
-            
-class Person(object):
-    def __init__(self,name,id_number,position):
-        
-        
-class Fellow(Person):
-    def __init__():
-        pass
-        #creates room
-        #has accomodation option
-        
-class Staff(Person):
-    def __init__():
-        pass
-        #can create office
-        
-class Room(object):
-    def __init():
-        pass
-        #can either be office or LS
-        
-class Office(Room):
-    def __init__():
-        pass
-        # maximum of 6
-        
-class Living_Space(Room):
-    def __init__():
-        pass
-        #maximum of 4
-        
-            
-        
-        
-        
         
