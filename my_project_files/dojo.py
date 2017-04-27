6b718692dbb148012bfcbc6eef1606ba0b2134a2
Deborah Agumeneitwe <dagumeneitwe@gmail.com>
2017-04-27 14:26:30 +0300

    Functionality for the add_person tests

--- my_project_files/dojo.py
+++ my_project_files/dojo.py
@@ -4,18 +4,18 @@ from person import Person, Staff, Fellow
 class Dojo(object):
     def __init__(self):
         self.all_rooms = {}
-        self.People = []
         self.initial_room_count = 0
         self.office_rooms = []
         self.living_rooms = []
         self.new_room_count = self.initial_room_count + len(self.all_rooms)
+        self.staff = []
+        self.fellows = []
+        self.people = self.fellows + self.staff
         
         
         
     def create_room(self,room_name,room_type):
         new_room = Room(room_name,room_type,0)
-                    
-        #for room in self.all_rooms:
                     
         if new_room.room_name not in self.all_rooms.keys(): 
                 #new_room = Room(room_name,room_type)
@@ -29,8 +29,7 @@ class Dojo(object):
                         self.living_rooms.append(new_room)
 
                 else:
-                    return ("Room type has to be either living or office")
-                
+                    return ("Room type has to be either living or office")     
         else:
             return("Please choose another name")
 
@@ -38,15 +37,24 @@ class Dojo(object):
         self.new_room_count ++ 1
         
         
-"""       
+   
+    
     def add_person(self,full_name,position):
-        person = Person
-        accomodation option = N
-        People.append(person)
-        Raise ("Neil has been successfully added")
+        person = Person(full_name,position)
         
-        if position == fellow and accomodation_option == N:            
-"""
+        if person.position == "staff":
+            self.staff.append(person)
+            print("staff successfully added")
+            
+        elif person.position == "fellow":
+            self.fellows.append(person)
+            print("fellow successfully added")
+            
+            #if living_option == "Y":
+                
+        
+        
+        #if position == fellow and accomodation_option == N:           
 
         
            
